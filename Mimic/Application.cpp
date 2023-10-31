9a03c9b0ddba2df95fa036c15f3c83abebe6e201
ehorlocker <ehorlocker@gmail.com>
2023-10-30 20:16:08 -0400

    layers added, example layer added to sandbox

--- Mimic/Application.cpp
+++ Mimic/Application.cpp
@@ -15,16 +15,35 @@ namespace Mimic {
 
 	}
 
+	void Application::PushLayer(Layer* layer) {
+		m_LayerStack.PushLayer(layer);
+	}
+
+	void Application::PushOverlay(Layer* layer) {
+		m_LayerStack.PushOverlay(layer);
+	}
+
 	void Application::OnEvent(Event& e) {
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
 		MZ_CORE_INFO("{0}", e);
+
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();) {
+			(*--it)->OnEvent(e);
+			if (e.Handled) {
+				break;
+			}
+		}
 	}
 
 	void Application::Run() {
 		while (m_Running) {
 			m_Window->OnUpdate();
+
+			for (Layer* layer : m_LayerStack) {
+				layer->OnUpdate();
+			}
 		}
 	}
 
